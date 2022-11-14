ff6a8599e1246a23726847cb0a6ae1bf50a041ec
ishita221209 <84296284+ishita221209@users.noreply.github.com>
2022-11-14 16:41:48 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-function preload() {
+nose_x= "";
+nose_y="";
 
-}
+function preload() {}
 
 function setup() {
     canvas = createCanvas(400, 400);
     canvas.center();
-    canvas.background("purple");
-    video = createCapture(VIDEO);
-    video.hide();
+    video1 = createCapture(VIDEO);
+    video1.hide();
+    poseNet = ml5.poseNet(video1,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
 
 function draw() {
-
+    image(video1, 0, 0, 400, 400);
+    canvas.background("purple");
 }
 
 function take_pic() {
     save("mypic.png");
+}
+
+function gotPoses(results){
+
+    console.log(results);
+    nose_x = results[0].pose.nose.x;
+    nose_y = results[0].pose.nose.y;
+}
+
+function modelLoaded(){
+    console.log("Model is Loaded.");
 }
